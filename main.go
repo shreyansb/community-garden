5be19c7c964bf65bf6a5ffd9a27b6bd8308a223c
Shreyans Bhansali <shreyans@venmo.com>
2012-09-03 18:05:56 -0400

    go fmt

--- main.go
+++ main.go
@@ -1,59 +1,61 @@
 package main
 
 import (
-    "code.google.com/p/go.net/websocket"
-    "flag"
-    "html/template"
-    "net/http"
-    "log"
-    "os"
-    "strings"
+	"code.google.com/p/go.net/websocket"
+	"flag"
+	"html/template"
+	"log"
+	"net/http"
+	"os"
+	"runtime"
+	"strings"
 )
 
-var port = flag.String("port", ":8080", "port")
-var homeTemplate = template.Must(template.ParseFiles("templates/home.html"))
-var staticFilePath = "/static/"
-
-///
-/// handlers
-///
-
-func homeHandler(response http.ResponseWriter, request *http.Request) {
-    // pass the `Host` so the app knows which websocket to connect to
-    homeTemplate.Execute(response, request.Host)
-}
-
-func staticHandler(response http.ResponseWriter, request *http.Request) {
-    cwd, err := os.Getwd()
-    if err != nil {
-        log.Fatal("staticHandler: ", err)
-        panic(err)
-    }
-    filePath := cwd + staticFilePath + strings.SplitN(request.RequestURI, "/", 3)[2]
-    http.ServeFile(response, request, filePath)
-}
+var (
+	port           = flag.String("port", ":8080", "port")
+	homeTemplate   = template.Must(template.ParseFiles("templates/home.html"))
+	staticFilePath = "/static/"
+	handlers       = map[string]func(http.ResponseWriter, *http.Request){
+		"/":        homeHandler,
+		"/static/": staticHandler,
+	}
+)
 
 ///
 /// main
 ///
 
 func main() {
-    // set up a map of routes to handler functions
-    handlers := map[string] func(http.ResponseWriter, *http.Request) () { 
-        "/"         : homeHandler, 
-        "/static/"  : staticHandler,
-    }
+	// set up a map of routes to handler functions
+	// set up all handlers
+	for route, handler := range handlers {
+		http.HandleFunc(route, handler)
+	}
+	http.Handle("/ws", websocket.Handler(wsHandler))
 
-    // set up all handlers
-    for route, handler := range handlers {
-        http.HandleFunc(route, handler)
-    }
-    http.Handle("/ws", websocket.Handler(wsHandler))
+	// start the server after getting either a user-entered or default :port
+	flag.Parse()
+	log.Printf("ListenAndServe: starting on localhost%s", *port)
+	if err := http.ListenAndServe(*port, nil); err != nil {
+		log.Fatal("ListenAndServe: ", err)
+	}
+}
 
-    // start the server after getting either a user-entered or default :port
-    flag.Parse()
-    log.Printf("ListenAndServe: starting on localhost%s", *port)
-    if err := http.ListenAndServe(*port, nil); err != nil {
-        log.Fatal("ListenAndServe: ", err)
-    }
+///
+/// handlers
+///
+
+func homeHandler(response http.ResponseWriter, request *http.Request) {
+	// pass the `Host` so the app knows which websocket to connect to
+	homeTemplate.Execute(response, request.Host)
+}
+
+func staticHandler(response http.ResponseWriter, request *http.Request) {
+	cwd, err := os.Getwd()
+	if err != nil {
+		log.Fatal("staticHandler: ", err)
+		panic(err)
+	}
+	filePath := cwd + staticFilePath + strings.SplitN(request.RequestURI, "/", 3)[2]
+	http.ServeFile(response, request, filePath)
 }
--- ws.go
+++ ws.go
@@ -1,53 +1,56 @@
 package main
 
 import (
-    "code.google.com/p/go.net/websocket"
-    "log"
-    "math/rand"
-    "time"
+	"code.google.com/p/go.net/websocket"
+	"log"
+	"math/rand"
+	"runtime"
+	"time"
 )
 
 type wsConnection struct {
-    ws *websocket.Conn
-    wsChan chan string
-    id string
+	ws     *websocket.Conn
+	wsChan chan string
+	id     string
 }
 
 type wsMessage struct {
-    Id string
-    Message string
+	Id      string
+	Message string
 }
 
 func wsHandler(ws *websocket.Conn) {
-    // create a wsConnection instance
-    wsConn := &wsConnection{
-        ws: ws,
-        wsChan: make(chan string),
-        id: randomId(),
-    }
+	log.Printf("%d goroutines running (via wsHandler)", runtime.NumGoroutine())
+	// create a wsConnection instance
+	wsConn := &wsConnection{
+		ws:     ws,
+		wsChan: make(chan string),
+		id:     randomId(),
+	}
 
-    // receive a message and respond to it in a goroutine
-    for {
-        var message wsMessage
-        err := websocket.JSON.Receive(wsConn.ws, &message)
-        if err != nil {
-            log.Printf("handleMessages Receive error: %v", err)
-            break
-        }
-        go respondToMessage(wsConn, message)
-    }
+	// receive a message and respond to it in a goroutine
+	for {
+		var message wsMessage
+		err := websocket.JSON.Receive(wsConn.ws, &message)
+		if err != nil {
+			log.Printf("handleMessages Receive error: %v", err)
+			break
+		}
+		go respondToMessage(wsConn, message)
+	}
 }
 
 func respondToMessage(wsConn *wsConnection, message wsMessage) {
-    /* Respond to an incoming message.
-    Right now, simulates work by sleeping for a few seconds
-    */
-    log.Printf("%v is processing a message", wsConn.id)
-    time.Sleep(time.Duration(rand.Int31n(5000)) * time.Millisecond)
-    log.Printf("%v responded: %s", wsConn.id, message)
-    err := websocket.JSON.Send(wsConn.ws, message)
-    if err != nil {
-        log.Printf("handleMessages Send error: %v", err)
-        return
-    }
+	/* Respond to an incoming message.
+	   Right now, simulates work by sleeping for a few seconds
+	*/
+	log.Printf("%d goroutines running (via respondToMessage)", runtime.NumGoroutine())
+	log.Printf("%v is processing a message", wsConn.id)
+	time.Sleep(time.Duration(rand.Int31n(5000)) * time.Millisecond)
+	log.Printf("%v responded: %s", wsConn.id, message)
+	err := websocket.JSON.Send(wsConn.ws, message)
+	if err != nil {
+		log.Printf("handleMessages Send error: %v", err)
+		return
+	}
 }
